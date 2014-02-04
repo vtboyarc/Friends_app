1813a6aaf28f67378de3abf9fa1921fb7cae1826
Rob G <robthefrog@gmail.com>
2014-02-04 13:48:57 -0600

    Fix broken unit testing when typing 'rake test'

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class StatusesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
     @status = statuses(:one)
   end
@@ -12,13 +14,14 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    @request.env["devise.mapping"] = Devise.mappings[:user]
     get :new
     assert_response :success
   end
 
   test "should create status" do
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content, name: @status.name }
+      post :create, status: { content: @status.content }
     end
 
     assert_redirected_to status_path(assigns(:status))
@@ -35,7 +38,7 @@ class StatusesControllerTest < ActionController::TestCase
   end
 
   test "should update status" do
-    patch :update, id: @status, status: { content: @status.content, name: @status.name }
+    patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
 
