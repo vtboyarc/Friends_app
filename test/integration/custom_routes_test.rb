ecb26e3911f3fa94bf9ce03096528e4987940c30
Rob G <robthefrog@gmail.com>
2014-02-04 22:41:46 -0600

    Finished through "Integration Tests" segment.

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+  test "/login route should open login page" do
+  	get '/login'
+  	assert_response :success
+  end
+
+  test "/logout route should open logout page" do
+  	get '/logout'
+  	assert_response :redirect
+  	assert_redirected_to "/"
+  end
+
+  test "/register route should open sign up page" do
+  	get '/register'
+  	assert_response :success
+  end
+end
