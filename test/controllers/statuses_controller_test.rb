a824080fdbbb26222c534e115cee9839d650744a
Rob G <robthefrog@gmail.com>
2014-02-05 21:49:31 -0600

    Finished through "Testing the Statuses Controller" segment

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -6,6 +6,12 @@ class StatusesControllerTest < ActionController::TestCase
     @status = statuses(:one)
   end
 
+  test "should be logged in to post a status" do
+    post :create, status: { content: "Hello"}
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+  
   test "should get index" do
     get :index
     assert_response :success
@@ -24,7 +30,8 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should create status" do
+  test "should create status when logged in" do
+    sign_in users(:rob)
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
@@ -37,12 +44,26 @@ class StatusesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should be logged in to get edit" do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should get edit when logged in" do
+    sign_in users(:rob)
     get :edit, id: @status
     assert_response :success
   end
 
-  test "should update status" do
+  test "should be logged in to update" do
+    patch :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "should update status when logged in" do
+    sign_in users(:rob)
     patch :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
   end
