5e9123287b723c401f9c7ebe3c93feca3d5dcc86
Rob G <robthefrog@gmail.com>
2014-02-05 22:17:26 -0600

    Fix bug in status controller test left over from adding user_id validation

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -33,7 +33,7 @@ class StatusesControllerTest < ActionController::TestCase
   test "should create status when logged in" do
     sign_in users(:rob)
     assert_difference('Status.count') do
-      post :create, status: { content: @status.content }
+      post :create, status: { content: @status.content, user_id: @status.user }
     end
 
     assert_redirected_to status_path(assigns(:status))
