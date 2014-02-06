8e71426ab152cbae377c2785c58977c524e4404f
Rob G <robthefrog@gmail.com>
2014-02-05 22:08:25 -0600

    Finished through "Testing Statuses" segment

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,6 @@
 class Status < ActiveRecord::Base
 	belongs_to :user
+
+	validates :content, { presence: :true, length: { minimum: 2 }}
+	validates :user, { presence: true }
 end
--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "status should require content" do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?   
+	end
+
+  test "status' content is at least 2 long" do
+    status = Status.new(content: '1')
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+
+  test "status' should have a user id" do
+    status = Status.new(content: "Hello")
+    assert !status.save
+    assert !status.errors[:user].empty?
+  end
 end
