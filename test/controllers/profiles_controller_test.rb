a9352fb1feb1d11a4751b8dfd217c5787d05f4ef
Rob G <robthefrog@gmail.com>
2014-02-06 17:54:26 -0600

    Finished through "Creating the Profiles Controller" segment

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, id: users(:rob).profile_name
     assert_response :success
+    assert_template "profiles/show"
   end
 
+  test "should show not found if user not found" do
+  	get :show, id: "doesn't exist"
+  	assert_response :not_found
+  end
 end
