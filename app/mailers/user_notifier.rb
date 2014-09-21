08aaccde8bd9a29d66fef461793ae5360b5ebe1e
Adam Carter <adamrcarter@gmail.com>
2014-09-21 15:33:05 -0500

    wip

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -10,4 +10,15 @@ class UserNotifier < ActionMailer::Base
     mail to: @friend.email,
     subject: "#{@user.full_name} wants to be your friend on My Friends! Aw!"
   end
+  
+  def friend_request_accepted(user_friendship_id)
+    user_friendship = UserFriendship.find(user_friendship_id)
+    
+    @user = user_friendship.user
+    @friend = user_friendship.friend
+    
+    mail to: @user.email,
+    subject: "#{@friend.full_name} has accepted your friend request! Aw!"
+  end
+  
 end
