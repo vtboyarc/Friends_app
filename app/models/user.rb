dc1213dfb726c04cfe5af9a21385888761547789
Adam Carter <adamrcarter@gmail.com>
2014-09-21 15:48:18 -0500

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,15 @@ class User < ActiveRecord::Base
 
   has_many :statuses
   has_many :user_friendships
-  has_many :friends, through: :user_friendships
+  has_many :friends, through: :user_friendships,
+            conditions: {user_friendships: {state: 'accepted'}}
+            
+  has_many :pending_user_friendships, class_name: 'UserFriendship',
+         foreign_key: :user_id,
+         conditions: {state: 'pending'}
+         
+  has_many :pending_friends, through: :pending_user_friendships, source: :friend
+  
 
   validates :first_name, presence: true
   validates :last_name, presence: true
