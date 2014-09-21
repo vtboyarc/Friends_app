4ae92b9f4e040418cdf515544e7d756842b63dda
Adam Carter <adamrcarter@gmail.com>
2014-09-21 14:46:21 -0500

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20140216022824) do
+  
 
   create_table "statuses", force: true do |t|
     t.text     "content"
