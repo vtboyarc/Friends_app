ce627210835e7318c11e4f9ba4c564e742bd9099
Adam Carter <adamrcarter@gmail.com>
2014-09-22 10:15:02 -0500

    wip

--- config/environment.rb
+++ config/environment.rb
@@ -10,8 +10,8 @@ Pony.options = {
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :domain => 'heroku.com',
-    :user_name => ENV['vtboyarc'],
-    :password => ENV['tinuviel5'],
+    :user_name => ENV[''],
+    :password => ENV[''],
     :authentication => :plain,
     :enable_starttls_auto => true
   }
